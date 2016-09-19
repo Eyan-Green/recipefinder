6868a702e46f525dc08b9743be1ff1feb212344a
Eyan-Green <ian.green736@btinternet.com>
2016-09-19 14:29:00 +0100

    add scss to table

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,11 @@
-class Recipe 
-	include HTTParty
-	ENV ["FOOD2FORK_KEY"] = 'db761cbf1207ba07c9218a20df52787d'
-	base_uri 'http://food2fork.com/api'
-	default_params key: ENV["FOOD2FORK_KEY"]
-	format :json
-
-	def self.for term
-		get("/search", query: { q: term})["recipes"]
-	end
-end 
+class Recipe
+ include HTTParty
+ ENV["FOOD2FORK_KEY"] = 'db761cbf1207ba07c9218a20df52787d'
+ base_uri 'http://food2fork.com/api/search'
+ default_params key: ENV["FOOD2FORK_KEY"]
+ format :json
+ 
+ def self.for term
+ get("/search", query: { q: term})["recipes"]
+ end
+end
