2b84423039058619140f33dc29e4e543fe55da86
Eyan-Green <ian.green736@btinternet.com>
2016-09-19 15:55:24 +0100

    base_uri includes value of f2f env var

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,11 @@
 class Recipe
  include HTTParty
-  
- key_value 	= ENV['FOOD2FORK_KEY']
- hostport 	= ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
- base_uri "http://#{hostport}/api"
  
- default_params key: key_value
+ hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+ user_key = ENV['FOOD2FORK_KEY'] 
+ 
+ base_uri "http://#{hostport}/api"
+ default_params key: user_key
  format :json
  
  def self.for term
