a81562c58d9ad714c4809d910d42f25536dec67b
Eyan-Green <ian.green736@btinternet.com>
2016-09-19 15:33:42 +0100

    update model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,9 @@ class Recipe
  include HTTParty
   
  key_value 	= ENV['FOOD2FORK_KEY']
- hostport 	= ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com/api/'
+ hostport 	= ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+ base_uri "http://#{hostport}/api"
  
-
- base_uri 'http://food2fork.com/api/'
  default_params key: key_value
  format :json
  
