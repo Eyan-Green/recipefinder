df6c12c695ac427bdedbcb977c6f8d709293d569
Eyan-Green <ian.green736@btinternet.com>
2016-09-19 15:10:13 +0100

    change base_uri

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe
  include HTTParty
   
- hostport 	= ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+ hostport 	= ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com/api/'
  key_value 	= ENV['FOOD2FORK_KEY']
- 
+
  base_uri 'http://food2fork.com/api/'
  default_params key: key_value
  format :json
